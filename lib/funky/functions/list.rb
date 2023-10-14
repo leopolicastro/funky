7e6c9d0a27b10faf7527eb05d5bf6dc6402beb31
elbpdev <lbpdev@pm.me>
2023-10-14 18:46:49 -0400

    add forward slash to end of functions path

--- lib/funky/functions/list.rb
+++ lib/funky/functions/list.rb
@@ -6,7 +6,8 @@ module Funky
   module Functions
     class List
       def self.call
-        file_path = "#{Funky.functions_path}list.yml"
+        file_path = "#{Funky.functions_path}/list.yml"
+
         if File.exist?(file_path)
           YAML.load_file(file_path)["functions"]
         else
