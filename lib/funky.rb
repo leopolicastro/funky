04254c1691fcc565afa3c7ba59267eef4a3136e9
elbpdev <lbpdev@pm.me>
2023-10-13 20:15:18 -0400

    first commit

---
+++ lib/funky.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "funky/version"
+
+module Funky
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/funky_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Funky do
+  it "has a version number" do
+    expect(Funky::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
